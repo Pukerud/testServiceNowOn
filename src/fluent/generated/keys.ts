1b1f9dcedd8df9d9584aaef5342cc53e408d32d8
Knut Jørgen Pukerud <knut.pukerud@onitio.com>
2025-11-04 10:24:45 +0100

    test

--- src/fluent/generated/keys.ts
+++ src/fluent/generated/keys.ts
@@ -4,6 +4,24 @@ declare global {
     namespace Now {
         namespace Internal {
             interface Keys extends KeysRegistry {
+                explicit: {
+                        "cs0": {
+                            "table": "sys_script_client",
+                            "id": "1eff8af0d4874cd6bc7f93d26f9a86a3"
+                        },
+                        "src_server_script_js": {
+                            "table": "sys_module",
+                            "id": "9d3f337515974c51bd1c715863d77e18"
+                        },
+                        "br0": {
+                            "table": "sys_script",
+                            "id": "72b68a0c638143908eb1569e0ba67f92"
+                        },
+                        "package_json": {
+                            "table": "sys_module",
+                            "id": "fac43f5481d64900ad94572becb5c483"
+                        }
+                    };
             }
         }
     }
