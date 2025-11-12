188d30011c0c301ccbe1c5b92302c2a8d51c0e8b
Knut Jørgen Pukerud <knut.pukerud@onitio.com>
2025-11-12 12:34:29 +0100

    commit nagging

---
+++ src/fluent/generated/sys_scope_privilege_f2944005838d3610c822f9847daad395.now.ts
@@ -0,0 +1,10 @@
+import { CrossScopePrivilege } from '@servicenow/sdk/core'
+
+export default CrossScopePrivilege({
+    $id: 'f2944005838d3610c822f9847daad395',
+    operation: 'read',
+    status: 'allowed',
+    target_name: 'sys_user',
+    target_scope: 'global',
+    target_type: 'sys_db_object',
+})
